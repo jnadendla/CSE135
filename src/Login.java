8ad7e161adc56ad3a1824d5725dda99465f8cc34
jnadendla <janarthan.nadendla@gmail.com>
2015-04-23 10:53:05 -0700

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -1,14 +1,18 @@
 import java.io.IOException;
 import java.io.PrintWriter;
- 
+import java.nio.channels.SeekableByteChannel;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 public class Login extends HttpServlet{
    
+   HttpSession session;
    UserDB udb = new UserDB();
    
    /**
@@ -16,14 +20,18 @@ public class Login extends HttpServlet{
     */
    @Override
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      session = request.getSession();
+      
       User found;
       System.out.println(request.getParameter("fname"));
       String name = request.getParameter("fname");
 
       found = udb.getUser(name);
       
-      if(found != null)
-         doPost(request, response);
+      if(found != null) {
+         session.setAttribute("user", found);
+         response.sendRedirect("Home.jsp");
+      }
    }
 
    /**
@@ -31,6 +39,6 @@ public class Login extends HttpServlet{
     */
    @Override
    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      response.sendRedirect("Home.jsp");
+      
    }
 }
