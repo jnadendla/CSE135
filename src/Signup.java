0fc53fe8cbce56f3a1462e3658ed9fa755cc34f4
Phoebe <phgao1994@gmail.com>
2015-04-22 17:20:32 -0700

    signup doGet() working

--- src/Signup.java
+++ src/Signup.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Signup {
+public class Signup extends HttpServlet{
 
    public boolean addUserToDB(String name, int age, String role, String state) {
       UserDB udb = new UserDB();
