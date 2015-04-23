b374f0cdfc7f21e0d4318ec69c5a636c0bde95aa
jnadendla <janarthan.nadendla@gmail.com>
2015-04-23 10:10:29 -0700

    Update UserDB.java

--- src/UserDB.java
+++ src/UserDB.java
@@ -16,6 +16,9 @@ public class UserDB {
 	  
       db.aquireConnection();
       try {
+      	 if(account == getUser(account.name))
+            return false;
+            
          PreparedStatement ps = db.connection.prepareStatement("INSERT INTO users "
                + "(name, age, role, state) VALUES (?,?,?,?)");
          ps.setString(1, account.name);
