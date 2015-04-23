4c0c4fa93604bdef438857db952b7f8751813a4e
jnadendla <janarthan.nadendla@gmail.com>
2015-04-22 23:40:31 -0700

    Update Signup.java

--- src/Signup.java
+++ src/Signup.java
@@ -8,32 +8,44 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Signup extends HttpServlet{
-
-	UserDB udb = new UserDB();
+   
+   UserDB udb = new UserDB();
    
    /**
     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
     */
+   @Override
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      System.out.println("Made it here");
-      
+      User fresh;
       String name = request.getParameter("fname");
       
       String strAge = request.getParameter("age");
+      if(strAge == null || strAge == "") {
+         doPost(request, response);
+         return;
+      }
+      
       int age = Integer.parseInt(strAge);
       
       String role = request.getParameter("role");
       String state = request.getParameter("state");
       
-      User u = new User(name, age, role, state);
+      if(name == null || role == null || state == null ||
+         name == "" || role == "" || state == "") {
+         doPost(request, response);
+         return;
+      }
       
-      udb.addUser(u); // add user  
+      fresh = new User(name, age, role, state);
+      
+      boolean success = udb.addUser(fresh);
    }
 
    /**
     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
     */
+   @Override
    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+      response.sendRedirect("ErrorSignup.jsp");
    }
 }
