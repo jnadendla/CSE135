d095e89253b0fe7a664813695115135ecfac761a
jnadendla <janarthan.nadendla@gmail.com>
2015-04-24 09:21:03 -0700

    Create StatesDB.java

---
+++ src/StatesDB.java
@@ -0,0 +1,55 @@
+import java.beans.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class StatesDB {
+   private DbConnection db;
+   
+   public StatesDB() {
+      db = new DbConnection();
+   }
+   
+   public int getStateName(int id) {
+      int state = 0;
+      try {
+         db.aquireConnection();
+         PreparedStatement ps = db.connection.prepareStatement("SELECT * FROM roles WHERE id = ?");
+         ps.setInt(1, id);
+         ResultSet result = ps.executeQuery();
+         result.next();
+         state = result.getInt(2);
+         
+         db.connection.commit();
+         db.connection.close();
+      } catch (SQLException e) {
+         System.out.println("Error reading role");
+         e.printStackTrace();
+      }      
+      
+      return state;
+   }
+   
+   
+   public int getStateID(String name) {
+      int state = 0;
+      try {
+         db.aquireConnection();
+         PreparedStatement ps = db.connection.prepareStatement("SELECT * FROM states WHERE name = ?");
+         ps.setString(1, name);
+         ResultSet result = ps.executeQuery();
+         result.next();
+         state = result.getInt(1);
+         
+         db.connection.commit();
+         db.connection.close();
+      } catch (SQLException e) {
+         System.out.println("Error reading role");
+         e.printStackTrace();
+      }      
+      
+      return state;
+   }
+   
+}
