65d86c3b5c1b9bb93dfef949a2f172ce7ae195f9
jnadendla <janarthan.nadendla@gmail.com>
2015-05-04 15:39:55 -0700

    Update ProductsDB.java

--- src/db/ProductsDB.java
+++ src/db/ProductsDB.java
@@ -1,70 +1,146 @@
 package db;
+
 import java.sql.*;
-import java.util.LinkedList;
 
-public class PurchaseDB {
-   private DbConnection db;
-   
-   public PurchaseDB() {
-      db = new DbConnection();
-      db.aquireConnection();
-   }
-   
-   public void insert(Purchase item) throws SQLException {
-      // Begin transaction
-      db.connection.setAutoCommit(false);
-      
-      // Create the prepared statement and use it to
-      // INSERT student values INTO the students table.
-      PreparedStatement pstmt = db.connection
-              .prepareStatement("INSERT INTO purchased (usr, product, quantity, price) VALUES (?, ?, ?, ?)");
+public class ProductsDB {
+    private DbConnection db;
 
-      pstmt.setInt(1, item.getUserId());
-      pstmt.setInt(2, item.getProductId());
-      pstmt.setInt(3, item.getQuantity());
-      pstmt.setDouble(4, item.getPrice());
-      
-      int rowCount = pstmt.executeUpdate();
+    public ProductsDB() {
+        db = new DbConnection();
+        db.aquireConnection();
+    }
 
-      // Commit transaction
-      db.connection.commit();
-      db.connection.setAutoCommit(true);
-   }
-   
-   public LinkedList<Purchase> getPurchases() throws SQLException {
-      // Create the statement
-      Statement statement = db.connection.createStatement();
-      LinkedList<Purchase> purchasedList = new LinkedList<Purchase>();
+    public ResultSet getProducts() throws SQLException {
+        // Create the statement
+        Statement statement = db.connection.createStatement();
 
-      // Use the created statement to SELECT
-      // the purchases attributes FROM the Purchase table.
-      ResultSet rs = statement.executeQuery("SELECT * FROM purchased");
-      
-      while(rs.next()) {
-         ProductsDB pdb = new ProductsDB();
-         CategoriesDB cdb = new CategoriesDB();
-         
-         int user = rs.getInt("usr");
-         int quantity = rs.getInt("quantity");
-         double price = rs.getDouble("price");
-         
-         int pid = rs.getInt("product");
-         ResultSet prod_set = pdb.getProduct(pid);
-         if(prod_set.next()) {
-            String name = prod_set.getString("name");
-            String sku = prod_set.getString("sku");
-            int categoryId = prod_set.getInt("category");
-            System.out.print(categoryId);
-            ResultSet cat_set = cdb.getCategory(categoryId);
-            if(cat_set.next()) {
-               String category = cat_set.getString("name");
-               Purchase p = new Purchase(user, pid, name, sku, category, quantity, price);
-               purchasedList.add(p);
-            }
-         }
-      }
+        // Use the created statement to SELECT
+        // the student attributes FROM the Student table.
+        ResultSet rs = statement.executeQuery("SELECT * FROM products");
 
-      return purchasedList;
-   }
+        return rs;
+    }
+    
+    public ResultSet getProduct(int id) throws SQLException {
+       // Create the statement
+       Statement statement = db.connection.createStatement();
 
+       // Use the created statement to SELECT
+       // the student attributes FROM the Student table.
+       ResultSet rs = statement
+               .executeQuery("SELECT * FROM products WHERE id = "
+                       + id);
+
+       return rs;
+    }
+
+    public ResultSet getProducts(String categoryId) throws SQLException {
+        // Create the statement
+        Statement statement = db.connection.createStatement();
+
+        // Use the created statement to SELECT
+        // the student attributes FROM the Student table.
+        ResultSet rs = statement
+                .executeQuery("SELECT * FROM products WHERE category = "
+                        + categoryId);
+
+        return rs;
+    }
+
+    public ResultSet getProducts(String categoryId, String name)
+            throws SQLException {
+        
+        System.out.println("catgoryId: " + categoryId);
+        System.out.println("name: " + name);
+        // Create the statement
+        Statement statement = db.connection.createStatement();
+
+        ResultSet rs = null;
+
+        if (categoryId != null && !categoryId.isEmpty() && name != null
+                && !name.isEmpty()) { // filter both
+            rs = statement
+                    .executeQuery("SELECT * FROM products WHERE category = "
+                            + categoryId + " AND name LIKE \'%" + name + "%\'");
+        } else if (categoryId != null && !categoryId.isEmpty()
+                && (name == null || name.isEmpty())) { // filter category
+            rs = statement
+                    .executeQuery("SELECT * FROM products WHERE category = "
+                            + categoryId);
+        } else if ((categoryId == null || categoryId.isEmpty()) && (name != null
+                && !name.isEmpty())) { // filter by name
+            System.out.println("filtering by name");
+            rs = statement
+                    .executeQuery("SELECT * FROM products WHERE name LIKE \'%"
+                            + name + "%\'");
+        } else { // no filters
+            rs = statement.executeQuery("SELECT * FROM products");
+        }
+
+        return rs;
+    }
+
+    public void insert(String name, String sku, String price, String category)
+            throws SQLException {
+        // Begin transaction
+        db.connection.setAutoCommit(false);
+
+        // Create the prepared statement and use it to
+        // INSERT student values INTO the students table.
+        PreparedStatement pstmt = db.connection
+                .prepareStatement("INSERT INTO products (name, sku, price, category) VALUES (?, ?, ?, ?)");
+
+        pstmt.setString(1, name);
+        pstmt.setString(2, sku);
+        pstmt.setObject(3, price, java.sql.Types.NUMERIC, 2);
+        pstmt.setInt(4, Integer.parseInt(category));
+
+        int rowCount = pstmt.executeUpdate();
+
+        // Commit transaction
+        db.connection.commit();
+        db.connection.setAutoCommit(true);
+    }
+
+    public void update(String name, String sku, String price, String category,
+            String id) throws SQLException {
+        // Begin transaction
+        db.connection.setAutoCommit(false);
+
+        // Create the prepared statement and use it to
+        // UPDATE student values in the Students table.
+        PreparedStatement pstmt = db.connection
+                .prepareStatement("UPDATE products SET name = ?, sku = ?, price = ?,"
+                        + " category = ? WHERE id = ?");
+
+        pstmt.setString(1, name);
+        pstmt.setString(2, sku);
+        pstmt.setObject(3, price, java.sql.Types.NUMERIC, 2);
+        pstmt.setInt(4, Integer.parseInt(category));
+        System.out.println(category);
+        pstmt.setInt(5, Integer.parseInt(id));
+
+        int rowCount = pstmt.executeUpdate();
+
+        // Commit transaction
+        db.connection.commit();
+        db.connection.setAutoCommit(true);
+    }
+
+    public void delete(String id) throws SQLException {
+        // Begin transaction
+        db.connection.setAutoCommit(false);
+
+        // Create the prepared statement and use it to
+        // DELETE students FROM the Students table.
+        PreparedStatement pstmt = db.connection
+                .prepareStatement("DELETE FROM products WHERE id = ?");
+
+        pstmt.setInt(1, Integer.parseInt(id));
+        int rowCount = pstmt.executeUpdate();
+
+        // Commit transaction
+        db.connection.commit();
+        db.connection.setAutoCommit(true);
+    }
 }
