b55fe4e8290e41ee0d270673f2153fd54ce95d7d
jnadendla <janarthan.nadendla@gmail.com>
2015-05-04 09:54:01 -0700

    Create PurchaseDB.java

---
+++ src/db/PurchaseDB.java
@@ -0,0 +1,33 @@
+package db;
+import java.sql.*;
+
+public class PurchasedDB {
+   private DbConnection db;
+   
+   public PurchasedDB() {
+      db = new DbConnection();
+      db.aquireConnection();
+   }
+   
+   public void insert(Purchase item) throws SQLException {
+      // Begin transaction
+      db.connection.setAutoCommit(false);
+      
+      // Create the prepared statement and use it to
+      // INSERT student values INTO the students table.
+      PreparedStatement pstmt = db.connection
+              .prepareStatement("INSERT INTO purchased (usr, product, quantity, price) VALUES (?, ?, ?, ?)");
+
+      pstmt.setInt(1, item.getUserId());
+      pstmt.setInt(2, item.getProductId());
+      pstmt.setInt(3, item.getQuantity());
+      pstmt.setDouble(4, item.getPrice());
+      
+      int rowCount = pstmt.executeUpdate();
+
+      // Commit transaction
+      db.connection.commit();
+      db.connection.setAutoCommit(true);
+   }
+
+}
